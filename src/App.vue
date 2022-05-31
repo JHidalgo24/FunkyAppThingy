c4819156cdd10b2bbc9338ccfb05c40a78154fd2
Hidxlgo <justinh8100@gmail.com>
2022-05-31 15:39:25 -0500

    fixed the footer and moved the mission statements

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 
     <v-main app>
         <router-view></router-view>
+
     </v-main>
 
     <v-navigation-drawer v-model="drawer" absolute temporary color="white">
@@ -26,41 +27,9 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-container>
-      <v-row>
-        <v-col cols sm="12" md="6" lg="6">
-          <v-card elevation="2">
-            <v-card-title>Why Join?</v-card-title>
-
-            <v-card-text>
-              Let's be honest here, how many times has you gone out with someone
-              and everything is going great. Good food, having some laughs everything
-              is breezy, until they bust out some wack ass music. Im talking real wack,
-              like you can't get jiggy with it no more. Well, now you can put those moments
-              behind you.
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col cols sm="12" md="6" lg="6">
-          <v-card elevation="2">
-            <v-card-title>What We Do</v-card-title>
-
-            <v-card-text>
-              Okay so you get why you should join but what exactly is the deal with music anyway?
-              Listen I don't know about you but geeking out over a specific band or song is super cool.
-              The vibes are there, you guys have a connection, having fun. Why? all because you both
-              have similar tastes in music. This app will pair people up based on what music they like.
-              We're talking genres, artist, songs, playlists.
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-
+    <v-spacer></v-spacer>
     <!--footer that messes up the cards above-->
-    <v-footer app color="black" >
+    <v-footer   color="black" >
       <v-row class="footer-items" >
         <v-col>
           <h1>bullshit one</h1>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,6 +8,38 @@
     >
       <h1>Tune into your soulmate</h1>
     </v-parallax>
+
+    <v-container>
+      <v-row>
+        <v-col cols sm="12" md="6" lg="6">
+          <v-card elevation="2">
+            <v-card-title>Why Join?</v-card-title>
+
+            <v-card-text>
+              Let's be honest here, how many times has you gone out with someone
+              and everything is going great. Good food, having some laughs everything
+              is breezy, until they bust out some wack ass music. Im talking real wack,
+              like you can't get jiggy with it no more. Well, now you can put those moments
+              behind you.
+            </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols sm="12" md="6" lg="6">
+          <v-card elevation="2">
+            <v-card-title>What We Do</v-card-title>
+
+            <v-card-text>
+              Okay so you get why you should join but what exactly is the deal with music anyway?
+              Listen I don't know about you but geeking out over a specific band or song is super cool.
+              The vibes are there, you guys have a connection, having fun. Why? all because you both
+              have similar tastes in music. This app will pair people up based on what music they like.
+              We're talking genres, artist, songs, playlists.
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
