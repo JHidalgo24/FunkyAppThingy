5cb1fdfbff1b4e750257d12bf63d587366100571
hidxlgo <justinh8100@gmail.com>
2022-06-11 17:16:34 -0500

    funky stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,22 +6,22 @@ import 'firebase/storage';
 //you need to enable authorization and firestore in testing mode, and it should work theoretically
 const firebaseConfig = {
     //Justins
-    // apiKey: "AIzaSyDeQpRWIbCiEcZrAA4eI2jWqjgehA4OI60",
-    // authDomain: "melodydating.firebaseapp.com",
-    // projectId: "melodydating",
-    // storageBucket: "melodydating.appspot.com",
-    // messagingSenderId: "874220047741",
-    // appId: "1:874220047741:web:14c96a3b0745e495251014",
-    // measurementId: "G-L2M71C6XSW"
+    apiKey: "AIzaSyDeQpRWIbCiEcZrAA4eI2jWqjgehA4OI60",
+    authDomain: "melodydating.firebaseapp.com",
+    projectId: "melodydating",
+    storageBucket: "melodydating.appspot.com",
+    messagingSenderId: "874220047741",
+    appId: "1:874220047741:web:14c96a3b0745e495251014",
+    measurementId: "G-L2M71C6XSW"
 
     //Davids
-    apiKey: "AIzaSyAwPtLGLf7Rx5LSVeLGVbSkzgWG0SezN94",
-    authDomain: "melody-7824c.firebaseapp.com",
-    projectId: "melody-7824c",
-    storageBucket: "melody-7824c.appspot.com",
-    messagingSenderId: "946301541272",
-    appId: "1:946301541272:web:bd426c12bd976a7060e9ed",
-    measurementId: "G-R74YJHGDG7"
+    // apiKey: "AIzaSyAwPtLGLf7Rx5LSVeLGVbSkzgWG0SezN94",
+    // authDomain: "melody-7824c.firebaseapp.com",
+    // projectId: "melody-7824c",
+    // storageBucket: "melody-7824c.appspot.com",
+    // messagingSenderId: "946301541272",
+    // appId: "1:946301541272:web:bd426c12bd976a7060e9ed",
+    // measurementId: "G-R74YJHGDG7"
 
 };
 
